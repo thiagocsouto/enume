86e3de17c410e583871257b5bf14f89bae49a625
Thiago Souto <thiagocsouto@outlook.com>
2022-07-14 11:48:18 -0400

    Delete enume directory

--- enume/src/main/java/com/curso/enume/repository/CursoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.curso.enume.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.curso.enume.entity.Curso;
-
-public interface CursoRepository extends JpaRepository<Curso, Integer> {
-
-}
