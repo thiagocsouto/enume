448bbf85754d9493b2f7bf71de065d12a8615789
thiagocsouto <thiagocsouto@outlook.com>
2022-08-09 14:37:15 -0400

    Commit no projeto

---
+++ src/main/java/com/curso/enume/entity/Curso.java
@@ -0,0 +1,66 @@
+package com.curso.enume.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name="curso")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Curso {
+
+	@Id 
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	
+	@Column(name="nome")
+	@NotNull
+	@Size(min=3, max=60, message="O nome deverá conter entre 3 e no máximo 50 caracteres para continuar.")
+	@Pattern(regexp = "^[A-Z][a-zA-ZáàâãéèêíïóôõöúçñÁÀÂÃÉÈÊÍÏÓÒÖÚÇÑ ]*$", message="O nome não deve começar com uma letra minúscula, número ou caractere especial.")
+	private String nome;
+	
+	@Column(name="descrição")
+//	@Size(min=10, max=200, message="A descrição do curso deverá conter pelo menos 10 para continuar.")
+	@NotNull
+	private String descricao;
+	 
+    @Column(name="quantidade_vagas")
+	@Pattern(regexp = "\\d+", message="Somente números.")
+	private String quantidadeVaga;
+    
+    @Column(name="carga_horaria")
+	@Pattern(regexp = "\\d+", message="Somente números.") 
+	private String cargaHoraria;
+    
+	@Column(name="valor")
+	@NotNull
+	private String valor;
+
+	public Curso(
+			@Size(min = 3, max = 60, message = "O nome deverá conter entre 3 e no máximo 50 caracteres para continuar.") @Pattern(regexp = "^[A-Z][a-zA-ZáàâãéèêíïóôõöúçñÁÀÂÃÉÈÊÍÏÓÒÖÚÇÑ ]*$", message = "O nome não deve começar com uma letra minúscula, número ou caractere especial.") String nome,
+			@Size(min = 10, max = 200, message = "A descrição do curso deverá conter pelo menos 10 para continuar.") String descricao,
+			@Pattern(regexp = "\\d+", message = "Somente números.") String quantidadeVaga,
+			@Pattern(regexp = "\\d+", message = "Somente números.") String cargaHoraria, String valor) {
+		super();
+		this.nome = nome;
+		this.descricao = descricao;
+		this.quantidadeVaga = quantidadeVaga;
+		this.cargaHoraria = cargaHoraria;
+		this.valor = valor;
+	} 
+	
+	
+	
+}
---
+++ src/test/java/com/curso/enume/FuncionarioServiceTest.java
@@ -0,0 +1,90 @@
+package com.curso.enume;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.when;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+
+import org.junit.jupiter.api.Assertions;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import com.curso.enume.entity.Funcionario;
+import com.curso.enume.exception.NegocioExcpetion;
+import com.curso.enume.repository.FuncionarioRepository;
+import com.curso.enume.service.FuncionarioService;
+
+
+@SpringBootTest
+public class FuncionarioServiceTest {
+
+	@Autowired
+	private FuncionarioService funcionarioService;
+	
+	@Mock
+	private FuncionarioRepository funcionarioRepository;
+	
+	@Test
+	void testeSalvarFuncionario() throws Exception {
+		Funcionario funcionario = new Funcionario("thiago Antonio", "Rh", "F005425", "2.000,00", "manaus", "thi", "11", "rua baixo", "lirios", "AM", "69038-050");
+		
+		Funcionario funcionarioSalvar = funcionarioService.salvarFuncionarios(funcionario);
+		
+		assertNotNull(funcionarioSalvar);		
+		
+	}
+	
+//	@Test
+//	void testeSalvarFuncionarioRepository() throws Exception {
+//		Funcionario funcionario = new Funcionario("Thiago Antonio", "RH", "F0054253", "2.000,00", "manaus", "thi", "11", "rua baixo", "lirios", "AM", "69038-050");
+//		
+//		when(funcionarioRepository.save(any())).thenReturn(funcionario);
+//		
+//		Funcionario funcionarioSalvar = funcionarioRepository.save(funcionario);
+//		
+//		assertNotNull(funcionarioSalvar);		
+//		
+//	}
+	
+//	@Test
+//	void testeIdNaoEncontradoFuncionario() throws Exception {
+//		   doReturn(Optional.empty()).when(funcionarioRepository).findById(1);
+//
+//	        Exception exception = assertThrows(NegocioExcpetion.class, ()-> funcionarioService.listarFuncionariosId(1)); 
+//
+//	        Assertions.assertEquals("Id não encontrado.", exception.getMessage());
+//	}
+	
+	@Test
+	void testeIdNaoPodeSerMenorIgualQueZeroAutor(){
+		  Exception exception = assertThrows(NegocioExcpetion.class, ()-> funcionarioService.listarFuncionariosId(0)); 
+
+	      Assertions.assertEquals("Id não pode ser menor ou igual a zero.", exception.getMessage());
+		
+	}
+	
+//	@Test
+//	void testeEncontrarTodosOsIdFuncionario() {
+//		Funcionario funcionario1 = new Funcionario("Thiago Antonio", "RH", "F005453", "2.000,00", "manaus", "thi", "11", "rua baixo", "altos", "AM", "69045-040");
+//		Funcionario funcionario2 = new Funcionario("Antonio Nunes", "TI", "F0050213", "8.000,00", "manaus", "cloves", "11", "rua manus", "lirios", "AM", "69018-052");
+//		Funcionario funcionario3 = new Funcionario("Carlos Lucca", "RH", "F0054253", "2.000,00", "manaus", "bweteer", "12", "rua silveira", "redendeção", "AM", "69038-012");
+//        doReturn(Arrays.asList(funcionario1, funcionario2, funcionario3)).when(funcionarioRepository).findAll();
+//        
+//        List<Funcionario> funcionarios = funcionarioService.listarFuncionarios();
+//        
+//        Assertions.assertEquals(3, funcionarios.size());
+//        
+//	}	
+	
+}
